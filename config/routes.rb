d6beb2891bde3f4cecc1819ea84df4e4547b0ecf
Brandon Caudillo <btcaudillo@gmail.com>
2023-04-04 10:13:06 -0600

    commit tree 01640e9a0a2510dd4e1d5dc862f6a93beba131ed

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   resources :members_only_articles, only: [:index, :show]
 
   get "/me", to: "users#show"
-
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 end
